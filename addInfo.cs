2daca7205aef3eaba05f88f88d73d2276af05d8b
828303 <49234429+828303@users.noreply.github.com>
2019-09-03 10:35:48 +0200

    Add files via upload

---
+++ addInfo.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GateBoys.Models
+{
+    public class addInfo
+    {
+
+        [Key]
+        public int MoreInfoId { get; set; }
+
+        [Display(Name = "Info of Email")]
+        public string addInfoOf { get; set; }
+
+        [Required]
+        [Display(Name = "Name")]
+        public string name { get; set; }
+
+
+        [Display(Name = "Middle Name")]
+        public string midName { get; set; }
+
+        [Required]
+        [Display(Name = "Surname")]
+        public string surname { get; set; }
+
+        [Display(Name = "ID Number")]
+        [Required]
+        [MaxLength(13)]
+        [MinLength(13)]
+        [Range(13, Int64.MaxValue, ErrorMessage = "ID Number should not contain charecters and must be 13 digits")]
+        public string idNum { get; set; }
+        
+
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        [MaxLength(10)]
+        [MinLength(10)]
+        [Range(10, Int64.MaxValue, ErrorMessage = "Phone number should not contain charecters and must be 10 digits")]
+        [Display(Name = "Phone Number")]
+        public string phone { get; set; }
+
+        public string dateRegistered { get; set; }
+
+        [Display(Name = "Country")]
+        public string country { get; set; }
+
+        [Display(Name = "Street Number")]
+        public string street_number { get; set; }
+
+
+        [Display(Name = "Street Name")]
+        public string route { get; set; }
+
+        [Display(Name = "Province")]
+        public string administrative_area_level_1 { get; set; }
+
+
+        [Display(Name = "City")]
+        public string locality { get; set; }
+
+        [Display(Name = "Code")]
+        public string postal_code { get; set; }
+
+        public string adress { get; set; }
+
+        public string addressCMBN()
+        {
+            string ad = (country + " " + street_number + " " + route + " " + administrative_area_level_1 + " " + locality + " " + postal_code);
+            return ad;
+        }
+
+
+        //public addInfo()
+        //{
+        //    this.bookingSomes = new HashSet<bookingSome>();
+        //    this.bookings = new HashSet<booking>();
+        //}
+
+        //public virtual ICollection<booking> bookings { get; set; }
+        //public virtual ICollection<bookingSome> bookingSomes { get; set; }
+
+    }
+}
---
+++ addInfoesController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using IdentitySample.Models;
+using Microsoft.AspNet.Identity;
+using GateBoys.Models;
+
+namespace GateBoys.Controllers
+{
+    public class addInfoesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: addInfoes
+        public ActionResult Index()
+        {
+            return View(db.addInfoes.ToList());
+        }
+
+        // GET: addInfoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            addInfo addInfo = db.addInfoes.Find(id);
+            if (addInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(addInfo);
+        }
+
+        // GET: addInfoes/Create
+        public ActionResult Create(string email)
+        {
+            if (email == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ViewBag.email = email;
+            return View();
+        }
+
+        // POST: addInfoes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MoreInfoId,addInfoOf,name,midName,surname,idNum,street_number,route,administrative_area_level_1,locality,country,postal_code,adress,phone,dateRegistered")] addInfo addInfo,string email)
+        {
+            var mail = User.Identity.GetUserName();
+            addInfo.addInfoOf = email;
+            addInfo.dateRegistered = DateTime.Now.ToString();
+            addInfo.adress = addInfo.addressCMBN();
+            if (ModelState.IsValid)
+            {
+                db.addInfoes.Add(addInfo);
+                db.SaveChanges();
+                FormsAuthentication.SignOut();
+                return RedirectToAction("success", new { id= addInfo.MoreInfoId });
+            }
+
+            return View(addInfo);
+        }
+
+        public ActionResult success(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var userInfo = db.addInfoes.ToList().Where(x => x.MoreInfoId == id).ToList();
+            if (userInfo == null)
+            {
+                return HttpNotFound();
+            }
+
+            else if (userInfo.Count < 1)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            else
+            {
+                foreach (var item in userInfo)
+                {
+                    ViewBag.naam = item.name+" "+item.midName;
+                    ViewBag.snaam = item.surname;
+                }
+            }
+            return View();
+        }
+
+        // GET: addInfoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            addInfo addInfo = db.addInfoes.Find(id);
+            if (addInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(addInfo);
+        }
+
+        // POST: addInfoes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MoreInfoId,addInfoOf,name,midName,surname,idNum,street_number,route,administrative_area_level_1,locality,country,postal_code,adress,phone,dateRegistered")] addInfo addInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(addInfo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(addInfo);
+        }
+
+        // GET: addInfoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            addInfo addInfo = db.addInfoes.Find(id);
+            if (addInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(addInfo);
+        }
+
+        // POST: addInfoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            addInfo addInfo = db.addInfoes.Find(id);
+            db.addInfoes.Remove(addInfo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
